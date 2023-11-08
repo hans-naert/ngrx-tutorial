1c7a3928e04e93f92cfc1e81fc07fc38a6f18a93
Hans Naert <hans.naert@vives.be>
2023-11-08 07:50:25 +0100

    use injected Store in my-counter for dispatching actions and selecting count$

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import {increment, decrement, reset} from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
@@ -10,20 +12,20 @@ export class MyCounterComponent {
 
   count$: Observable<number>
  
-  constructor() {
-    // TODO: Connect `this.count$` stream to the current store `count` state
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
   }
  
   increment() {
-    // TODO: Dispatch an increment action
+    this.store.dispatch(increment());
   }
  
   decrement() {
-    // TODO: Dispatch a decrement action
+    this.store.dispatch(decrement());
   }
  
   reset() {
-    // TODO: Dispatch a reset action
+    this.store.dispatch(reset());
   }
 
 }
